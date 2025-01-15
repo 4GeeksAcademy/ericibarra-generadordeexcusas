ce414f7262be4412c0243bb35710e1fe6be4710f
Asenius <100125344+Asenius@users.noreply.github.com>
2025-01-15 21:21:49 +0000

    todo hecho

--- src/app.js
+++ src/app.js
@@ -7,5 +7,56 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let form = document.getElementById("myForm");
+  let sendButton = document.getElementById("send");
+  let generateButton = document.getElementById("generate");
+
+  let who = [];
+  let action = [];
+  let what = [];
+  let when = [];
+
+  sendButton.addEventListener("click", function() {
+    let whoInput = document.getElementById("whoInput").value;
+    let actionInput = document.getElementById("actionInput").value;
+    let whatInput = document.getElementById("whatInput").value;
+    let whenInput = document.getElementById("whenInput").value;
+
+    if (!whoInput || !actionInput || !whatInput || !whenInput) {
+      alert("Por favor, completa todos los campos.");
+      return;
+    } else {
+      who.push(whoInput);
+      action.push(actionInput);
+      what.push(whatInput);
+      when.push(whenInput);
+    }
+
+    form.reset();
+  });
+
+  generateButton.addEventListener("click", function() {
+    if (!who.length || !action.length || !what.length || !when.length) {
+      alert("Por favor, completa todos los campos.");
+      return;
+    }
+
+    let firstRnd = Math.floor(Math.random() * who.length);
+    let secondRnd = Math.floor(Math.random() * action.length);
+    let thirdRnd = Math.floor(Math.random() * what.length);
+    let fourthRnd = Math.floor(Math.random() * when.length);
+
+    let finalExcuse =
+      who[firstRnd] +
+      " " +
+      action[secondRnd] +
+      " " +
+      what[thirdRnd] +
+      " " +
+      when[fourthRnd] +
+      ".";
+
+    document.getElementById("excuse").innerHTML = finalExcuse;
+  });
 };
